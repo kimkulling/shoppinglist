2121c17e86c7c4b54b51f0dc5465ee9da46050fb
Kim Kulling <kim.kulling@googlemail.com>
2016-05-06 16:48:01 +0200

    Fix processItems for space-separated items.

--- app/src/main/java/com/app/kimkulling/shoppinglist/ShoppingItem.java
+++ app/src/main/java/com/app/kimkulling/shoppinglist/ShoppingItem.java
@@ -1,55 +1,83 @@
 package com.app.kimkulling.shoppinglist;
 
 /**
- * Created by kimku_000 on 16.02.2016.
+ *  This class is used to define a bundle of shopping items.
  */
 public class ShoppingItem {
     String mShop;
     String mItems;
 
+    /**
+     * The class constructor.
+     * @param shop  The nae of the shop
+     * @param items The list of items
+     */
     public ShoppingItem( final String shop, final String items ) {
         mShop = shop;
         mItems = items;
     }
 
+    /**
+     * Set a new shop name.
+     * @param newShop   The new shop name.
+     */
     void setShop( final String newShop ) {
         this.mShop = newShop;
     }
 
+    /**
+     *  Returns the shop name.
+     * @return  The name of the shop.
+     */
     String getShop() {
         return mShop;
     }
 
+    /**
+     * Set the item list.
+     * @param items The item list as a single string.
+     */
     void setItems( final String items ) {
         mItems = items;
     }
 
+    /**
+     * Returns the item list.
+     * @return  The item list as a string.
+     */
     String getItems() {
         return mItems;
     }
 
-    static public String processItems( String items ) {
+    /**
+     * Item processor to get the items formatted in a nice way.
+     * @param items
+     * @return
+     */
+    static public String processItems( final String items ) {
         if ( 0 == items.length() ) {
             return items;
         }
 
-        String sevItems[] = items.split( "," ), proceededItems[] = null;
-        if ( null == sevItems ) {
-            String spaceItems[] = items.split( " " );
-            if ( null == spaceItems ) {
-                return items;
-            } else {
-                proceededItems = spaceItems;
+        String sep[] = { ",", " " }, proceededItems[] = null;
+        String regexp[] = { ",", "\\s+" };
+
+        for ( int i=0; i<sep.length; i++) {
+            if ( items.contains( sep[ i ] ) ) {
+                String sepItems[] = items.split(regexp[ i ] );
+                proceededItems = sepItems;
+                break;
             }
-        } else {
-            proceededItems = sevItems;
         }
 
         String newItems = new String();
-        for ( int i=0; i<proceededItems.length; i++ ) {
-            String currentItem = proceededItems[ i ];
-            currentItem = currentItem.trim() + "\n";
-            newItems = newItems + currentItem;
+        if ( null != proceededItems ) {
+            for ( String currentItem: proceededItems ) {
+                currentItem = currentItem.trim() + "\n";
+                newItems = newItems + currentItem;
+            }
+        } else {
+            newItems = items;
         }
 
         return newItems;
---
+++ app/src/test/java/com/app/kimkulling/shoppinglist/ShoppingItemUnitTest.java
@@ -0,0 +1,24 @@
+package com.app.kimkulling.shoppinglist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This class is used to test the shopping item class.
+ */
+public class ShoppingItemUnitTest {
+    @Test
+    public void separateStringByComma() throws Exception {
+        String items = new String("a, b, c, d" );
+        String proceededItems = ShoppingItem.processItems( items );
+        assertEquals( "a\nb\nc\nd\n", proceededItems );
+    }
+
+    @Test
+    public void separateStringBySpace() throws Exception {
+        String items = new String("a b c d" );
+        String proceededItems = ShoppingItem.processItems( items );
+        assertEquals( "a\nb\nc\nd\n", proceededItems );
+    }
+}
